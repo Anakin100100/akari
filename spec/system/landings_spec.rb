e3987db32836149efe3baf5c64b590376bac1e63
Paweł Biegun <biegunpawel900@gmail.com>
2021-11-07 21:35:38 +0100

    capybara, devise login helpers

---
+++ spec/system/landings_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe "Landings", type: :system do
+  before do
+    driven_by(:rack_test)
+  end
+
+  it "load the landing page" do
+    login_as(create(:teacher))
+
+    visit root_path
+
+    expect(page).to have_content("welcome to akari")
+  end
+  
+end
