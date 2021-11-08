308aeb35ef06577287a2950765eec0e855d635f1
Paweł Biegun <biegunpawel900@gmail.com>
2021-11-08 15:45:43 +0100

    capitalization of welcome to akari on landing page

--- spec/system/landings_spec.rb
+++ spec/system/landings_spec.rb
@@ -3,11 +3,11 @@
 require 'rails_helper'
 
 RSpec.describe 'Landings', type: :system do
-  it 'load the landing page', :js do
+  it 'load the landing page' do
     login_as(create(:teacher))
 
     visit root_path
 
-    expect(page).to have_content('WELCOME TO AKARI')
+    expect(page).to have_content('welcome to akari')
   end
 end
