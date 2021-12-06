8c7dbe619df2898ddd965e3a98a50d52efc779b3
Paweł Biegun <biegunpawel900@gmail.com>
2021-12-06 15:12:31 +0100

    autentykacja urzytkownika przed każdą akcją teacher i student controllerów

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class StudentsController < ApplicationController
+    before_action :authenticate_student!
+
     def student_dashboard
     end
 end
--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class TeachersController < ApplicationController
+    before_action :authenticate_teacher!
+
+
     def teacher_dashboard
     end
 end
