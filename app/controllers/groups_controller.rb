cb885c1b9ca8b21a03893d5b06c567d7ced0e26f
Paweł Biegun <biegunpawel900@gmail.com>
2021-12-06 20:56:46 +0100

    save

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -9,9 +9,23 @@ class GroupsController < ApplicationController
     end
 
     def create
-        @group = Group.new(group_params)
+        @group = Group.new(name: params["group"]["name"])
         @group.teacher_id = current_teacher.id
+        binding.pry
         if @group.save
+            invalid_names = []
+            params["group"]["students_names"].split("\n").each do |student_email|
+                if Student.where(email: student_email).empty?
+                    invalid_names << student_email
+                else
+                    @group.students << Student.where(email: student_email)
+                end
+            end
+            if invalid_names.empty? == false
+                flash[:Student_validation_error] = "The following students have invalid email addresses: #{invalid_names.join(', ')}"
+                redirect_to new_group_path
+                return
+            end
             redirect_to groups_path
         else
             render :new
