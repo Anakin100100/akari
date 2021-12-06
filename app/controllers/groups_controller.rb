bf40502e8e1d3ff027a9df2b2c184d655bbbb76d
Paweł Biegun <biegunpawel900@gmail.com>
2021-12-06 16:07:05 +0100

    save

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,5 +2,6 @@ class GroupsController < ApplicationController
     before_action :authenticate_teacher!
 
     def index
+        @groups = Group.all.where(teacher_id: current_teacher.id)
     end
 end
