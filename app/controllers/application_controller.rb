1a47d94096018e0d3d2c57d8ec973eef0f7c9b4e
Paweł Biegun <biegunpawel900@gmail.com>
2021-12-15 19:51:38 +0100

    zmiana rejestracji uczniów

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
     def after_sign_in_path_for(resource)
         if resource.is_a?(Teacher)
             teacher_dashboard_path # your path
@@ -8,4 +11,13 @@ class ApplicationController < ActionController::Base
             student_dashboard_path # your path
         end
     end
+
+    protected
+
+    def configure_permitted_parameters
+        attributes = [:name, :surname, :email]
+        devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
+        devise_parameter_sanitizer.permit(:account_update, keys: attributes)
+    end
+
 end
