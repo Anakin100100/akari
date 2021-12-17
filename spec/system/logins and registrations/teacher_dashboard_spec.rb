2a8ce7c57b439a802cbb943fcb61fcb15b8f1e0e
Paweł Biegun <biegunpawel900@gmail.com>
2021-12-17 15:09:40 +0100

    save

--- spec/system/logins and registrations/teacher_dashboard_spec.rb
+++ spec/system/logins and registrations/teacher_dashboard_spec.rb
@@ -3,10 +3,13 @@
 require 'rails_helper'
 
 RSpec.describe 'Teacher dashboard', type: :system do
-    it 'contains the groups button' do
+    it 'contains the groups button and redirects to groups page' do
         teacher = create(:teacher)
         sign_in teacher
         visit '/teacher_dashboard'
         expect(page).to have_content("Grupy")
+        click_link "Grupy"
+        expect(page).to have_current_path("/groups")
     end
+
 end
